8ea4d3ed5e2c5dcb67822d7debcaa4d397e1c0fd
arshcherbakov <shcherbakov.fortech@gmail.com>
2024-07-04 23:50:42 +0300

    feature: debounce

--- user-list/src/App.js
+++ user-list/src/App.js
@@ -1,11 +1,78 @@
+import { useEffect, useState } from "react";
+import { getUsers } from "./services/users-service";
 import "./App.css";
 
-function App() {
+// const searchData = (listData, searchData) => {
+//   useEffect(() => {
+//     const filterData = listData.filter((data) => {
+//       data.includes(searchData);
+//     });
+//   }, []);
+
+//   return
+// };
+
+const App = () => {
+  const [usersList, setUersList] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [page, setPage] = useState(1);
+
+  // useEffect(() => {
+  //   if (searchInput) {
+  //     const request = setTimeout(() => {
+  //       getUsers(searchInput, page).then((res) => {
+  //         setUersList(res.items);
+  //         console.log(res);
+  //       });
+  //     }, 1500);
+
+  //     return clearTimeout(request);
+  //   }
+  // }, [page]);
+
+  useEffect(() => {
+    if (searchInput) {
+      const request = setTimeout(() => {
+        getUsers(searchInput, page).then((res) => {
+          setUersList(res.items);
+        });
+      }, 1500);
+
+      return () => clearTimeout(request);
+    }
+  }, [searchInput]);
+
+  const debounce = (callback, interval) => {
+    setTimeout(() => {
+      callback();
+    }, interval);
+  };
+
+  const handlerSearchInput = (event) => {};
+
+  const handlerNextPage = () => {
+    setPage(page + 1);
+  };
+
+  const handlerPrevPage = () => {
+    if (page < 0) {
+      console.log(page);
+      setPage(page - 1);
+    }
+  };
+
   return (
     <div className="App">
-      <h1>Start</h1>
+      <h1>{searchInput}</h1>
+      <input onChange={(e) => setSearchInput(e.target.value)} />
+      {usersList.map((user) => (
+        <p key={user.id}>{user.login}</p>
+      ))}
+      <h3>{page}</h3>
+      <button onClick={handlerNextPage}>Дальше</button>
+      <button onClick={handlerPrevPage}>Назад</button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ user-list/src/services/users-service.js
@@ -0,0 +1,10 @@
+const getUsers = (seacrh, page = 1, options) => {
+  return fetch(
+    `https://api.github.com/search/users?q=${seacrh}&page=${page}`,
+    options
+  )
+    .then((res) => res.json())
+    .catch((err) => console.log("rr", err));
+};
+
+export { getUsers };
